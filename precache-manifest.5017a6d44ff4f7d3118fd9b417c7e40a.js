463802a898a7dd32b111e2ae48a71ea25f7ab792
Richard Skogsberg <rskogsberg@MacBook-Air-2.local>
2020-07-18 10:47:40 -0700

    Updates

---
+++ precache-manifest.5017a6d44ff4f7d3118fd9b417c7e40a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3cce8366cd9c92e4c7e4f25c1755a1",
+    "url": "/typescript-sorting-comparison/index.html"
+  },
+  {
+    "revision": "65b8642b47572c457404",
+    "url": "/typescript-sorting-comparison/static/css/main.8402de5f.chunk.css"
+  },
+  {
+    "revision": "eb063e6c50787b393fc5",
+    "url": "/typescript-sorting-comparison/static/js/2.f39af3a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typescript-sorting-comparison/static/js/2.f39af3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b8642b47572c457404",
+    "url": "/typescript-sorting-comparison/static/js/main.76a64f9b.chunk.js"
+  },
+  {
+    "revision": "93f9db8395ff69bff541",
+    "url": "/typescript-sorting-comparison/static/js/runtime-main.edcc1fa0.js"
+  }
+]);
